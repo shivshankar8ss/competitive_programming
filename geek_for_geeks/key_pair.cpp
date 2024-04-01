8a145bdc5a3981ad434c28ebbff5c62e81b22f88
shivshankar8ss <shivshankar8w@gmail.com>
2024-04-01 23:40:55 +0530

    gfg code

---
+++ geek_for_geeks/key_pair.cpp
@@ -0,0 +1,30 @@
+// just solution for code
+
+class Solution
+{
+public:
+    bool hasArrayTwoCandidates(int arr[], int n, int x)
+    {
+        sort(arr, arr + n);
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right)
+        {
+            int sum = arr[left] + arr[right];
+
+            if (arr[left] + arr[right] == x)
+            {
+                return true;
+            }
+            else if (sum > x)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+    }
+};
